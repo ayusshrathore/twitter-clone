293b602286762838182ab7d79762e867973d32e3
ayusshrathore <ayusshrathore@gmail.com>
2023-06-05 21:57:12 +0530

    chore: add page to show user via id

---
+++ pages/users/[userId].tsx
@@ -0,0 +1,32 @@
+import Header from "@/components/Header";
+import PostFeed from "@/components/Posts/PostFeed";
+import UserBio from "@/components/users/UserBio";
+import UserHero from "@/components/users/UserHero";
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+const UserView = () => {
+	const router = useRouter();
+	const { userId } = router.query;
+
+	const { data: fetchedUser, isLoading } = useUser(userId as string);
+
+	if (isLoading || !fetchedUser)
+		return (
+			<div className="flex justify-center items-center h-full">
+				<ClipLoader color="lightblue" size={80} />
+			</div>
+		);
+
+	return (
+		<>
+			<Header showBackArrow label={fetchedUser?.name} />
+			<UserHero userId={userId as string} />
+			<UserBio userId={userId as string} />
+			<PostFeed userId={userId as string} />
+		</>
+	);
+};
+
+export default UserView;
