db873233bdd9723090db6f75f8538430f1127cb4
ayusshrathore <ayusshrathore@gmail.com>
2023-06-05 21:53:31 +0530

    chore: cleanup default code & setup initial pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+
+import Layout from "@/components/Layout";
+import EditModal from "@/components/modals/EditModal";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<SessionProvider session={pageProps.session}>
+			<Toaster />
+			<LoginModal />
+			<RegisterModal />
+			<EditModal />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</SessionProvider>
+	);
 }
