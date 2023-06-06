ad324201d78f0ca7a2d9df1c5a387f69ce02fa4a
Ayush @ Classiebit <ayush.classiebit@gmail.com>
2023-06-06 17:30:40 +0530

    chore: fix text color in button

--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -3,76 +3,56 @@ import { AiOutlineClose } from "react-icons/ai";
 import Button from "../Button";
 
 interface ModalProps {
-	isOpen?: boolean;
-	onClose: () => void;
-	onSubmit: () => void;
-	title: string;
-	body?: React.ReactElement;
-	footer?: React.ReactElement;
-	actionLabel: string;
-	disabled?: boolean;
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
 }
 
-const Modal: FC<ModalProps> = ({
-	isOpen,
-	onClose,
-	onSubmit,
-	title,
-	body,
-	footer,
-	actionLabel,
-	disabled,
-}) => {
-	const handleClose = useCallback(() => {
-		if (disabled) return;
+const Modal: FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, footer, actionLabel, disabled }) => {
+  const handleClose = useCallback(() => {
+    if (disabled) return;
 
-		onClose();
-	}, [disabled, onClose]);
+    onClose();
+  }, [disabled, onClose]);
 
-	const handleSubmit = useCallback(() => {
-		if (disabled) return;
+  const handleSubmit = useCallback(() => {
+    if (disabled) return;
 
-		onSubmit();
-	}, [disabled, onSubmit]);
+    onSubmit();
+  }, [disabled, onSubmit]);
 
-	if (!isOpen) return null;
+  if (!isOpen) return null;
 
-	return (
-		<>
-			<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800 bg-opacity-70">
-				<div className="relative w-full lg:w-3/6 my-6 mx-auto lg:max-w-3xl h-full lg:h-auto">
-					{/* Content */}
-					<div className="h-full lg:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black outline-none focus:outline-none">
-						{/* Header */}
-						<div className="flex items-center justify-between p-10 rounded-t">
-							<h3 className="text-3xl font-semibold text-white">{title}</h3>
-							<button
-								onClick={handleClose}
-								className="p-1 ml-auto border-0 text-white hover:opacity-70 transition"
-							>
-								<AiOutlineClose size={20} />
-							</button>
-						</div>
-						{/* Body */}
-						<div className="relative p-10 flex-auto">{body}</div>
-						{/* Footer */}
-						<div className="flex flex-col gap-2 p-10">
-							<Button
-								disabled={disabled}
-								label={actionLabel}
-								secondary
-								fullWidth
-								large
-								onClick={handleSubmit}
-								outline
-							/>
-							{footer}
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800 bg-opacity-70">
+        <div className="relative w-full lg:w-3/6 my-6 mx-auto lg:max-w-3xl h-full lg:h-auto">
+          {/* Content */}
+          <div className="h-full lg:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black outline-none focus:outline-none">
+            {/* Header */}
+            <div className="flex items-center justify-between p-10 rounded-t">
+              <h3 className="text-3xl font-semibold text-white">{title}</h3>
+              <button onClick={handleClose} className="p-1 ml-auto border-0 text-white hover:opacity-70 transition">
+                <AiOutlineClose size={20} />
+              </button>
+            </div>
+            {/* Body */}
+            <div className="relative p-10 flex-auto">{body}</div>
+            {/* Footer */}
+            <div className="flex flex-col gap-2 p-10">
+              <Button secondary disabled={disabled} label={actionLabel} fullWidth large onClick={handleSubmit} />
+              {footer}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
