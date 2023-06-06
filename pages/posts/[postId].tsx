2511164ebe94c19bdeb958fd14e9cf06f8efc558
Ayush @ Classiebit <ayush.classiebit@gmail.com>
2023-06-06 17:32:30 +0530

    chore: show comments in feed

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,34 +1,32 @@
 import Form from "@/components/Form";
 import Header from "@/components/Header";
 import PostItem from "@/components/Posts/PostItem";
+import CommentFeed from "@/components/posts/CommentFeed";
 import usePost from "@/hooks/usePost";
 import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
 
 const PostView = () => {
-	const router = useRouter();
-	const { postId } = router.query;
+  const router = useRouter();
+  const { postId } = router.query;
 
-	const { data: fetchedPost, isLoading } = usePost(postId as string);
+  const { data: fetchedPost, isLoading } = usePost(postId as string);
 
-	if (isLoading || !fetchedPost)
-		return (
-			<div className="flex justify-center items-center h-full">
-				<ClipLoader color="lightblue" size={80} />
-			</div>
-		);
+  if (isLoading || !fetchedPost)
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
 
-	return (
-		<>
-			<Header showBackArrow label="Tweet" />
-			<PostItem data={fetchedPost} />
-			<Form
-				postId={postId as string}
-				isComment
-				placeholder="Tweet your reply"
-			/>
-		</>
-	);
+  return (
+    <>
+      <Header showBackArrow label="Tweet" />
+      <PostItem data={fetchedPost} />
+      <Form postId={postId as string} isComment placeholder="Tweet your reply" />
+      <CommentFeed comments={fetchedPost.comments} />
+    </>
+  );
 };
 
 export default PostView;
