44bf60b7832a512cbc11dc15f13b8470fa9a86b5
Ayush @ Classiebit <ayush.classiebit@gmail.com>
2023-06-06 17:32:59 +0530

    chore: refactor imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Form from "@/components/Form";
 import Header from "@/components/Header";
-import PostFeed from "@/components/Posts/PostFeed";
+import PostFeed from "@/components/posts/PostFeed";
 
 export default function Home() {
-	return (
-		<>
-			<Header label="Home" />
-			<Form placeholder="What's happening?" />
-			<PostFeed />
-		</>
-	);
+  return (
+    <>
+      <Header label="Home" />
+      <Form placeholder="What's happening?" />
+      <PostFeed />
+    </>
+  );
 }
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import PostFeed from "@/components/Posts/PostFeed";
+import PostFeed from "@/components/posts/PostFeed";
 import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/UserHero";
 import useUser from "@/hooks/useUser";
@@ -7,26 +7,26 @@ import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
 
 const UserView = () => {
-	const router = useRouter();
-	const { userId } = router.query;
+  const router = useRouter();
+  const { userId } = router.query;
 
-	const { data: fetchedUser, isLoading } = useUser(userId as string);
+  const { data: fetchedUser, isLoading } = useUser(userId as string);
 
-	if (isLoading || !fetchedUser)
-		return (
-			<div className="flex justify-center items-center h-full">
-				<ClipLoader color="lightblue" size={80} />
-			</div>
-		);
+  if (isLoading || !fetchedUser)
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
 
-	return (
-		<>
-			<Header showBackArrow label={fetchedUser?.name} />
-			<UserHero userId={userId as string} />
-			<UserBio userId={userId as string} />
-			<PostFeed userId={userId as string} />
-		</>
-	);
+  return (
+    <>
+      <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
+    </>
+  );
 };
 
 export default UserView;
